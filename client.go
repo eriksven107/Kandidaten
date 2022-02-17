30231af9af2504648cce27f567fc3a829c25a9be
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2022-02-17 16:22:21 +0100

    Add files via upload

--- client.go
+++ client.go
@@ -3,10 +3,7 @@ package main
 import (
   "fmt"
   "os"
-  "bytes"
-  "encoding/pem"
   "crypto/tls"
-//  "crypto/x509"
 )
 
 func main() {
@@ -18,32 +15,18 @@ func main() {
       return
   }
 
-  var encodedCert bytes.Buffer
-  err = pem.Encode(&encodedCert, &pem.Block{
-    Type: "CERTIFICATE",
-    Bytes: conn.ConnectionState().PeerCertificates[0].Raw,
-  })
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
 
-/*  p, err := x509.MarshalPKIXPublicKey(conn.ConnectionState().PeerCertificates[0].PublicKey)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  publicKey := string(pem.EncodeToMemory(&pem.Block{
-    Type:  "PUBLIC KEY",
-    Bytes: p,
-  }))*/
+  fmt.Print("Embedded SCT?")
 
-  fmt.Print("A")
-  fmt.Print(conn.ConnectionState().PeerCertificates[0])
-  fmt.Print("B")
-  fmt.Print(conn.ConnectionState().SignedCertificateTimestamps)
-  fmt.Print("C")
-  fmt.Print(conn.ConnectionState().OCSPResponse)
+    fmt.Print(conn.ConnectionState().PeerCertificates[0].Extensions)
+
+  fmt.Print("As a TLS extension in the handshake?")
+
+    fmt.Print(conn.ConnectionState().SignedCertificateTimestamps)
+
+  fmt.Print("OCSP Stapeling?")
+
+    fmt.Print(conn.ConnectionState().OCSPResponse)
 
 
 }
