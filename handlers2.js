41e4cb8ac79fb6ab7b20b9d217074e75cfda1e8e
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2022-05-06 16:11:23 +0200

    Create handlers2.js

---
+++ handlers2.js
@@ -0,0 +1,139 @@
+// 	User Agent Normal: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0 Selenium:
+
+import * as asn1js from 'asn1js';
+import { fromBase64, stringToArrayBuffer} from 'pvutils';
+import { Certificate } from 'pkijs';
+
+let start;
+
+const getX509Ext = (extensions, v) => {
+  for (var extension in extensions) {
+    if (extensions[extension].extnID === v) {
+      return extensions[extension];
+    }
+  }
+
+  return {
+    extnValue: undefined,
+    parsedValue: undefined,
+  };
+
+};
+
+
+export const parse = async (certificate,issuerkeyhash) => {
+
+  // parse the certificate
+  const asn1 = asn1js.fromBER(certificate);
+
+
+  let x509 = new Certificate({ schema: asn1.result });
+
+  x509 = x509.toJSON()
+
+  // get the embedded SCTs
+  let scts = getX509Ext(x509.extensions, '1.3.6.1.4.1.11129.2.4.2').parsedValue;
+
+let data=[];
+if(!(scts === undefined)) {
+  data[0]="SCTs"
+  data[1]=scts;
+  data[2]="\n tbs:"
+  data[3]=x509['tbs']
+  data[4]="\n issuerkeyhash:";
+  data[5]=issuerkeyhash;
+console.log("\nBefore sending SCT:"+(Date.now()-start)/1000;
+  const request = new Request('http://localhost:1895/', {method: 'POST',headers:{'Content-Type': 'application/json'} ,body: JSON.stringify(data)});
+ fetch(request);
+ console.log("\nAfter sending SCT:"+(Date.now()-start)/1000);
+}
+};
+
+
+
+
+export const pemToBER = (pem) => {
+  return stringToArrayBuffer(window.atob(pem));
+};
+
+// returns an object with a certs array: it either inserts them into an existing securityInfo
+// object, or it creates a new object containing only the certs
+export const buildChain = async (chain) => {
+  let builtChain;
+
+  let issuerkeyhash=chain[1]['subjectPublicKeyInfoDigest'];
+
+
+  // probably a PEM encoded certificate
+  if (typeof chain === 'string' && chain.includes('-----BEGIN CERTIFICATE-----')) {
+    builtChain = chain.trim()
+                      .replace(/\r|\n|\0/g, '')
+                      .split(/-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----/g)
+                      .filter(v => v.startsWith('MII'));
+
+    builtChain = builtChain.map(cert => { return pemToBER(cert) });
+    //console.log(builtChain);
+  } else if (chain.buffer) {   // DER encoded
+    builtChain = [ chain.buffer ];
+  } else if (typeof chain === 'object' && Array.isArray(chain)) {
+    builtChain = chain.map(cert => { return new Uint8Array(cert.rawDER).buffer });
+  }
+  else {console.log("else");}
+
+  // now we need to parse each of the certificates, and return the parsed chain
+  return await Promise.all(builtChain.map(cert => parse(cert,issuerkeyhash)));
+};
+
+
+export const consume = async details => {
+
+  const mainFrame = details.type === 'main_frame';
+  let securityInfo;
+
+  // only pull security info on top level requests
+  if (mainFrame) {
+
+
+    // grab the security info
+    securityInfo = await browser.webRequest.getSecurityInfo( // fetch the security info
+      details.requestId,
+      { certificateChain: true, rawDER: true }); //Prova med certificateChain: false, bara första borde ha sct?
+
+    if (securityInfo !== undefined) {
+      // sometimes securityInfo doesn't return keaGroupName for whatever reason
+      if (!securityInfo.keaGroupName) {
+        securityInfo.keaGroupName = undefined;
+      }
+
+      buildChain(securityInfo['certificates']);
+
+    return;
+  }
+
+   }
+};
+
+
+
+browser.webRequest.onHeadersReceived.addListener(
+  details => { consume(details); start=Date.now();
+
+
+
+  },
+  { urls: ['<all_urls>'] },
+  ['blocking'],
+
+
+);
+
+browser.webRequest.onCompleted.addListener(
+  details => { console.log("\nOnload:"+(Date.now()-start)/1000);
+
+
+  },
+  { urls: ['<all_urls>'] },
+
+
+
+);
